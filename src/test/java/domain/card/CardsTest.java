7c7ee6bd89434b194393aebb70edb651f1598296
limheesang <dlagmltkd12@naver.com>
2022-12-02 19:35:17 +0900

    test: 스코어가 10일 때 Ace 카드 추가 시 11로 추가

--- src/test/java/domain/card/CardsTest.java
+++ src/test/java/domain/card/CardsTest.java
@@ -15,40 +15,45 @@ class CardsTest {
     @BeforeEach
     void setUp() {
         Card clovarTwo = new Card(Symbol.TWO, Type.CLOVAR);
-        Card clovarKing = new Card(Symbol.KING, Type.CLOVAR);
+        Card clovarKing = new Card(Symbol.EIGHT, Type.CLOVAR);
         cards = new Cards(newArrayList(clovarTwo, clovarKing));
     }
 
     @Test
     void 카드의_합을_계산() {
-
-        assertThat(cards.calculateSum()).isEqualTo(12);
+        assertThat(cards.calculateSum()).isEqualTo(10);
     }
 
     @Test
     void 카드합이_주어진_숫자와_같은지() {
-        assertThat(cards.isSameScore(12)).isTrue();
+        assertThat(cards.isSameScore(10)).isTrue();
     }
 
     @Test
     void 카드합이_주어진_숫자보다_큰지() {
-        assertThat(cards.isBiggerScore(10)).isTrue();
+        assertThat(cards.isBiggerScore(9)).isTrue();
     }
 
     @Test
     void 카드합이_주어진_숫자보다_작은지() {
-        assertThat(cards.isSmallerScore(13)).isTrue();
+        assertThat(cards.isSmallerScore(11)).isTrue();
     }
 
     @Test
     void 카드한장_추가() {
         cards.add(new Card(Symbol.FIVE, Type.DIAMOND));
-        assertThat(cards.calculateSum()).isEqualTo(17);
+        assertThat(cards.calculateSum()).isEqualTo(15);
     }
 
     @Test
     void 블랙잭인지_확인() {
         Cards cards = new Cards(List.of(new Card(Symbol.ACE, Type.DIAMOND), new Card(Symbol.KING, Type.DIAMOND)));
         assertThat(cards.isBlackJack()).isTrue();
+    }
+
+    @Test
+    void 스코어가10일떄_Ace추가시_11로추가() {
+        cards.add(new Card(Symbol.ACE, Type.DIAMOND));
+        assertThat(cards.isSameScore(21)).isTrue();
     }
 }
